4cabac402076004751c90fc29c2719b1f21ff8b2
Atul Shahu <atul.shahu@cfins.com>
2022-04-11 15:51:59 +0000

    updated policty

--- models/cfins/policy.sql
+++ models/cfins/policy.sql
@@ -2,20 +2,28 @@
     materialized ='table'
 )}}
 
-select DIRASS Business_Type,
-date(nullifzero(EFFDTE),'YYYYMMDD') Policy_Effective_Date,
-date(nullifzero(EXPDTE),'YYYYMMDD')  Policy_Expiration_Date,
+select 
 POLICYNUM Policy_Number,
 RTEMTD Source_System_ID,
-CO Writing_Company_ID
+CO Writing_Company_ID,
+DIRASS Business_Type,
+date(nullifzero(EFFDTE),'YYYYMMDD') Policy_Effective_Date,
+date(nullifzero(EXPDTE),'YYYYMMDD')  Policy_Expiration_Date,
+FIELDF Product_specification_key,
+'NA' Policy_Term_ID
+
 FROM 
 CF_PreStaging.Public.DWXP010
 UNION ALL
-select DIRASS Business_Type,
-date(nullifzero(EFFDTE),'YYYYMMDD') Policy_Effective_Date,
-date(nullifzero(EXPDTE),'YYYYMMDD')  Policy_Expiration_Date,
+select 
 POLICYNUM Policy_Number,
 RTEMTD Source_System_ID,
-CO Writing_Company_ID
+CO Writing_Company_ID,
+DIRASS Business_Type,
+date(nullifzero(EFFDTE),'YYYYMMDD') Policy_Effective_Date,
+date(nullifzero(EXPDTE),'YYYYMMDD')  Policy_Expiration_Date,
+FIELDF Product_specification_key,
+'NA' Policy_Term_ID
+
 FROM 
 CF_PreStaging.Public.CF2FLT150P_DWXP010

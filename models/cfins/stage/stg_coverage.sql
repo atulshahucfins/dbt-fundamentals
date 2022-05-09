e2a77d5ec3f728e1a2221226bb6b888ea2888c89
Atul Shahu <atul.shahu@cfins.com>
2022-05-09 15:11:31 +0000

    updated stg_coverage

--- models/cfins/stage/stg_coverage.sql
+++ models/cfins/stage/stg_coverage.sql
@@ -3,42 +3,57 @@
     tags =["tag_in_code"]
 )}}
 
-
-select concat('FCW',cast ((ltrim(POLICY,0)) as varchar(50)),'||', cast ((ltrim(EFFDTE,0)) as varchar(50))) as PolicyKey,
- concat('FCW',cast ((ltrim(POLICY,0)) as varchar(50)),'||',
-              cast ((ltrim(EFFDTE,0)) as varchar(50)),'||',
-             ltrim(LOCNUM,0),'||',
-             ltrim(BLDNUM,0),'||',
-             ltrim(PRMSTE,0),'||',
-             ltrim(CLASX,0),'||',
-             ltrim(SUBLN,0),'||',
-             ltrim(INTCOV,0)) as Coverage_Key ,
-date(nullifzero(EFFDTE),'YYYYMMDD')  Coverage_Effective_Dt,
-date(nullifzero(COVEND),'YYYYMMDD')  Coverage_End_Dt,
-date(nullifzero(EXPDTE),'YYYYMMDD')  Coverage_Expiration_Dt,
-'NA' Coverage_Code_ID,
-'NA' Coverage_ID,
+with coverage_fc as (
+select concat('FCW',cast ((ltrim(c1.POLICY,0)) as varchar(50)),'||', cast ((ltrim(c1.EFFDTE,0)) as varchar(50))) as Policy_Key,
+ concat('FCW',cast ((ltrim(c1.POLICY,0)) as varchar(50)),'||',
+              cast ((ltrim(c1.EFFDTE,0)) as varchar(50)),'||',
+             ltrim(c1.LOCNUM,0),'||',
+             ltrim(c1.BLDNUM,0),'||',
+             ltrim(c1.PRMSTE,0),'||',
+             ltrim(c1.CLASX,0),'||',
+             ltrim(c1.SUBLN,0),'||',
+             ltrim(c1.INTCOV,0)) as Coverage_Key ,
+date(nullifzero(c1.EFFDTE),'YYYYMMDD')  Coverage_Effective_Dt,
+date(nullifzero(c1.COVEND),'YYYYMMDD')  Coverage_End_Dt,
+date(nullifzero(c1.EXPDTE),'YYYYMMDD')  Coverage_Expiration_Dt,
+c2.coverage_code_id Coverage_Code_ID,
 'NA' Coverage_Start_Dt,
 'NA' Active_Ind,
 'NA'Transaction_Key
           
-from CF_PreStaging.Public.DWXP050 
-UNION ALL
-select concat('CFW',cast ((ltrim(POLICY,0)) as varchar(50)),'||', cast ((ltrim(EFFDTE,0)) as varchar(50))) as PolicyKey,
- concat('CFW',cast ((ltrim(POLICY,0)) as varchar(50)),'||',
-              cast ((ltrim(EFFDTE,0)) as varchar(50)),'||',
-             ltrim(LOCNUM,0),'||',
-             ltrim(BLDNUM,0),'||',
-             ltrim(PRMSTE,0),'||',
-             ltrim(CLASX,0),'||',
-             ltrim(SUBLN,0),'||',
-             ltrim(INTCOV,0)) as Coverage_Key ,
-date(nullifzero(EFFDTE),'YYYYMMDD')  Coverage_Effective_Dt,
-date(nullifzero(COVEND),'YYYYMMDD')  Coverage_End_Dt,
-date(nullifzero(EXPDTE),'YYYYMMDD')  Coverage_Expiration_Dt,
-'NA' Coverage_Code_ID,
-'NA' Coverage_ID,
+from CF_PreStaging.Public.DWXP050 c1
+left join "DBT_STAGE"."DBT_ASHAHU"."STG_COVERAGE_CODE" c2
+on concat(trim(c1.PROD),'||',trim(c1.INTCOV)) = C2.coverage_code
+and c2.coverage_source ='FC_WINS'
+),
+
+coverage_cf as(
+select concat('CFW',cast ((ltrim(c1.POLICY,0)) as varchar(50)),'||', cast ((ltrim(c1.EFFDTE,0)) as varchar(50))) as Policy_Key,
+ concat('CFW',cast ((ltrim(c1.POLICY,0)) as varchar(50)),'||',
+              cast ((ltrim(c1.EFFDTE,0)) as varchar(50)),'||',
+             ltrim(c1.LOCNUM,0),'||',
+             ltrim(c1.BLDNUM,0),'||',
+             ltrim(c1.PRMSTE,0),'||',
+             ltrim(c1.CLASX,0),'||',
+             ltrim(c1.SUBLN,0),'||',
+             ltrim(c1.INTCOV,0)) as Coverage_Key ,
+date(nullifzero(c1.EFFDTE),'YYYYMMDD')  Coverage_Effective_Dt,
+date(nullifzero(c1.COVEND),'YYYYMMDD')  Coverage_End_Dt,
+date(nullifzero(c1.EXPDTE),'YYYYMMDD')  Coverage_Expiration_Dt,
+c2.coverage_code_id Coverage_Code_ID,
 'NA' Coverage_Start_Dt,
 'NA' Active_Ind,
 'NA'Transaction_Key           
-from CF_PreStaging.Public.CF2FLT150P_DWXP050
+from CF_PreStaging.Public.CF2FLT150P_DWXP050 c1
+left join "DBT_STAGE"."DBT_ASHAHU"."STG_COVERAGE_CODE" c2
+on concat(trim(c1.PROD),'||',trim(c1.INTCOV)) = C2.coverage_code
+and c2.coverage_source ='CF_WINS'
+)
+select sha2(coverage_key) coverage_id, policy_key,  coverage_key, coverage_code_id, coverage_effective_dt, 
+coverage_end_dt,coverage_Expiration_dt,coverage_Start_dt,active_ind, transaction_key from
+coverage_fc
+UNION ALL
+select sha2(coverage_key) coverage_id, policy_key,  coverage_key, coverage_code_id, coverage_effective_dt, 
+coverage_end_dt,coverage_Expiration_dt,coverage_Start_dt,active_ind, transaction_key
+from coverage_cf
+           

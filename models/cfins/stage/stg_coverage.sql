f5f8c3f5a3a1954a23bcc79264d52243b1abd916
Atul Shahu <atul.shahu@cfins.com>
2022-05-24 14:16:24 +0000

    updating stg_policy and stg_coverage code

--- models/cfins/stage/stg_coverage.sql
+++ models/cfins/stage/stg_coverage.sql
@@ -4,15 +4,13 @@
 )}}
 
 with coverage_fc as (
-select concat('FCW',cast ((ltrim(c1.POLICY,0)) as varchar(50)),'||', cast ((ltrim(c1.EFFDTE,0)) as varchar(50))) as Policy_Key,
- concat('FCW',cast ((ltrim(c1.POLICY,0)) as varchar(50)),'||',
-              cast ((ltrim(c1.EFFDTE,0)) as varchar(50)),'||',
-             ltrim(c1.LOCNUM,0),'||',
-             ltrim(c1.BLDNUM,0),'||',
-             ltrim(c1.PRMSTE,0),'||',
-             ltrim(c1.CLASX,0),'||',
-             ltrim(c1.SUBLN,0),'||',
-             ltrim(c1.INTCOV,0)) as Coverage_Key ,
+select ltrim(POLICY,0) Policy_Number,
+ltrim(c1.LOCNUM,0) location_number,
+ltrim(c1.BLDNUM,0) building_number,
+ltrim(c1.PRMSTE,0) PRMSTE,
+ltrim(c1.CLASX,0) CLASX,
+ltrim(c1.SUBLN,0) SUBLN,
+ltrim(c1.INTCOV,0) internal_coverage,
 date(nullifzero(c1.EFFDTE),'YYYYMMDD')  Coverage_Effective_Dt,
 date(nullifzero(c1.COVEND),'YYYYMMDD')  Coverage_End_Dt,
 date(nullifzero(c1.EXPDTE),'YYYYMMDD')  Coverage_Expiration_Dt,
@@ -28,15 +26,13 @@ and c2.coverage_source ='FC_WINS'
 ),
 
 coverage_cf as(
-select concat('CFW',cast ((ltrim(c1.POLICY,0)) as varchar(50)),'||', cast ((ltrim(c1.EFFDTE,0)) as varchar(50))) as Policy_Key,
- concat('CFW',cast ((ltrim(c1.POLICY,0)) as varchar(50)),'||',
-              cast ((ltrim(c1.EFFDTE,0)) as varchar(50)),'||',
-             ltrim(c1.LOCNUM,0),'||',
-             ltrim(c1.BLDNUM,0),'||',
-             ltrim(c1.PRMSTE,0),'||',
-             ltrim(c1.CLASX,0),'||',
-             ltrim(c1.SUBLN,0),'||',
-             ltrim(c1.INTCOV,0)) as Coverage_Key ,
+select ltrim(POLICY,0) Policy_Number,
+ltrim(c1.LOCNUM,0) location_number,
+ltrim(c1.BLDNUM,0) building_number,
+ltrim(c1.PRMSTE,0) PRMSTE,
+ltrim(c1.CLASX,0) CLASX,
+ltrim(c1.SUBLN,0) SUBLN,
+ltrim(c1.INTCOV,0) internal_coverage,
 date(nullifzero(c1.EFFDTE),'YYYYMMDD')  Coverage_Effective_Dt,
 date(nullifzero(c1.COVEND),'YYYYMMDD')  Coverage_End_Dt,
 date(nullifzero(c1.EXPDTE),'YYYYMMDD')  Coverage_Expiration_Dt,
@@ -49,11 +45,17 @@ left join {{ref('stg_coverage_code')}} c2
 on concat(trim(c1.PROD),'||',trim(c1.INTCOV)) = C2.coverage_code
 and c2.coverage_source ='CF_WINS'
 )
-select sha2(coverage_key) coverage_id, policy_key,  coverage_key, coverage_code_id, coverage_effective_dt, 
-coverage_end_dt,coverage_Expiration_dt,coverage_Start_dt,active_ind, transaction_key from
-coverage_fc
+select sha2(concat('FCW','||',Policy_Number,'||',
+              Coverage_Effective_Dt,'||',building_number,'||',PRMSTE,'||', CLASX,'||', SUBLN,'||',internal_coverage) ) coverage_id, 
+        concat('FCW','||',Policy_Number,'||',Coverage_Effective_Dt) policy_key,  concat('CFW','||',Policy_Number,'||',
+              Coverage_Effective_Dt,'||',building_number,'||',PRMSTE,'||', CLASX,'||', SUBLN,'||',internal_coverage) Coverage_Key , 
+        coverage_code_id, coverage_effective_dt,coverage_end_dt,coverage_Expiration_dt,coverage_Start_dt,active_ind, transaction_key 
+from coverage_fc
 UNION ALL
-select sha2(coverage_key) coverage_id, policy_key,  coverage_key, coverage_code_id, coverage_effective_dt, 
-coverage_end_dt,coverage_Expiration_dt,coverage_Start_dt,active_ind, transaction_key
+select sha2(concat('CFW','||',Policy_Number,'||',
+              Coverage_Effective_Dt,'||',building_number,'||',PRMSTE,'||', CLASX,'||', SUBLN,'||',internal_coverage) ) coverage_id, 
+        concat('CFW','||',Policy_Number,'||',Coverage_Effective_Dt) policy_key,  concat('CFW','||',Policy_Number,'||',
+              Coverage_Effective_Dt,'||',building_number,'||',PRMSTE,'||', CLASX,'||', SUBLN,'||',internal_coverage) Coverage_Key , coverage_code_id, coverage_effective_dt, 
+        coverage_end_dt,coverage_Expiration_dt,coverage_Start_dt,active_ind, transaction_key
 from coverage_cf
            
--- models/cfins/stage/stg_policy.sql
+++ models/cfins/stage/stg_policy.sql
@@ -11,7 +11,9 @@ CO Writing_Company_ID,
 DIRASS Business_Type,
 date(nullifzero(EFFDTE),'YYYYMMDD') Policy_Effective_Date,
 date(nullifzero(EXPDTE),'YYYYMMDD')  Policy_Expiration_Date,
-POLTRM Policy_Term
+POLTRM Policy_Term,
+try_to_timestamp_ntz(concat(TRDATE::varchar,' ',TRTIME::varchar),'YYYYMMDD HH24MISSFF9') Policy_Start_Dt
+
 
 FROM 
  {{source('PRD_CAESAR_RL_DB_WINSFC','DWXP010')}} ),
@@ -25,15 +27,16 @@ DIRASS Business_Type,
 date(nullifzero(EFFDTE),'YYYYMMDD') Policy_Effective_Date,
 date(nullifzero(EXPDTE),'YYYYMMDD')  Policy_Expiration_Date,
 FIELDF Product_specification_key,
-POLTRM Policy_Term
+POLTRM Policy_Term,
+try_to_timestamp_ntz(concat(TRDATE::varchar,' ',TRTIME::varchar),'YYYYMMDD HH24MISSFF9') Policy_Start_Dt
 
 FROM 
 {{source('PRD_CAESAR_RL_DB_WINSCF','DWXP010')}})
 
-select concat('FCW','|',Policy_Number,'|',Policy_Effective_Date) Product_specification_key,Policy_Number,Source_System_ID,Writing_Company_ID,Business_Type,
-Policy_Effective_Date,Policy_Expiration_Date,Policy_Term from policy_fcw
+select concat('FCW','||',Policy_Number,'||',Policy_Effective_Date) Product_specification_key,Policy_Number,Source_System_ID,Writing_Company_ID,Business_Type,
+Policy_Effective_Date,Policy_Expiration_Date,Policy_Term,Policy_Start_Dt from policy_fcw
 
 UNION ALL
 
-select concat('CFW','|',Policy_Number,'|',Policy_Effective_Date) Product_specification_key,Policy_Number,Source_System_ID,Writing_Company_ID,Business_Type,
-Policy_Effective_Date,Policy_Expiration_Date,Policy_Term from policy_cfw
+select concat('CFW','||',Policy_Number,'||',Policy_Effective_Date) Product_specification_key,Policy_Number,Source_System_ID,Writing_Company_ID,Business_Type,
+Policy_Effective_Date,Policy_Expiration_Date,Policy_Term,Policy_Start_Dt from policy_cfw
